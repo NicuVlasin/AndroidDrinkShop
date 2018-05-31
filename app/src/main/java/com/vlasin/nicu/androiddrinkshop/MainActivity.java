f46e318daf09eaa2009dcf8b8b554849b8bedf45
Nicu Vlasin <nicu_vlasin@yahoo.com>
2018-05-31 16:10:06 +0100

    Facebook Account Kit and Register Account

--- app/src/main/java/com/vlasin/nicu/androiddrinkshop/MainActivity.java
+++ app/src/main/java/com/vlasin/nicu/androiddrinkshop/MainActivity.java
@@ -1,13 +1,239 @@
 package com.vlasin.nicu.androiddrinkshop;
 
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Base64;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.facebook.accountkit.Account;
+import com.facebook.accountkit.AccountKit;
+import com.facebook.accountkit.AccountKitCallback;
+import com.facebook.accountkit.AccountKitError;
+import com.facebook.accountkit.AccountKitLoginResult;
+import com.facebook.accountkit.ui.AccountKitActivity;
+import com.facebook.accountkit.ui.AccountKitConfiguration;
+import com.facebook.accountkit.ui.LoginType;
+import com.rengwuxian.materialedittext.MaterialEditText;
+import com.szagurskii.patternedtextwatcher.PatternedTextWatcher;
+import com.vlasin.nicu.androiddrinkshop.Model.CheckUserResponse;
+import com.vlasin.nicu.androiddrinkshop.Model.User;
+import com.vlasin.nicu.androiddrinkshop.Retrofit.IDrinkShopAPI;
+import com.vlasin.nicu.androiddrinkshop.Utils.Common;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+import dmax.dialog.SpotsDialog;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUEST_CODE = 1000;
+    Button btn_continue;
+
+    IDrinkShopAPI mService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mService = Common.getAPI();
+
+        btn_continue = findViewById(R.id.btn_continue);
+        btn_continue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startLoginPage(LoginType.PHONE);
+            }
+        });
+
+        //printKeyHash();
+    }
+
+    private void startLoginPage(LoginType loginType) {
+        Intent intent = new Intent(this, AccountKitActivity.class);
+        AccountKitConfiguration.AccountKitConfigurationBuilder builder =
+                new AccountKitConfiguration.AccountKitConfigurationBuilder(loginType,
+                        AccountKitActivity.ResponseType.TOKEN);
+        intent.putExtra(AccountKitActivity.ACCOUNT_KIT_ACTIVITY_CONFIGURATION,builder.build());
+        startActivityForResult(intent, REQUEST_CODE);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CODE)
+        {
+            AccountKitLoginResult result = data.getParcelableExtra(AccountKitLoginResult.RESULT_KEY);
+
+            if (result.getError() != null)
+            {
+                Toast.makeText(this, "" + result.getError().getErrorType().getMessage(), Toast.LENGTH_SHORT).show();
+            }
+            else if (result.wasCancelled())
+            {
+                Toast.makeText(this, "Cancel", Toast.LENGTH_SHORT).show();
+            }
+            else
+            {
+                if (result.getAccessToken() != null)
+                {
+                    final android.app.AlertDialog alertDialog = new SpotsDialog(MainActivity.this);
+                    alertDialog.show();
+                    alertDialog.setMessage("Loading...");
+
+                    //Get User phone and check if exists on server
+                    AccountKit.getCurrentAccount(new AccountKitCallback<Account>() {
+                        @Override
+                        public void onSuccess(final Account account) {
+                            mService.checkUserExists(account.getPhoneNumber().toString())
+                                    .enqueue(new Callback<CheckUserResponse>() {
+                                        @Override
+                                        public void onResponse(Call<CheckUserResponse> call, Response<CheckUserResponse> response) {
+                                            CheckUserResponse userResponse = response.body();
+                                            if (userResponse.isExists())
+                                            {
+                                                //If User already exists, just start new Activity
+                                                alertDialog.dismiss();
+
+                                            }
+                                            else
+                                            {
+                                                //If User dose not exist then it needs to register
+                                                alertDialog.dismiss();
+
+                                                showRegisterDialog(account.getPhoneNumber().toString());
+                                            }
+                                        }
+
+                                        @Override
+                                        public void onFailure(Call<CheckUserResponse> call, Throwable t) {
+
+                                        }
+                                    });
+                        }
+
+                        @Override
+                        public void onError(AccountKitError accountKitError) {
+                            Log.d("ERROR", accountKitError.getErrorType().getMessage());
+
+                        }
+                    });
+                }
+            }
+        }
+    }
+
+    private void showRegisterDialog(final String phone) {
+        /*
+            I will create a new layout for custom dialog (register dialog)
+         */
+        final AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setTitle("REGISTER");
+
+
+        LayoutInflater inflater = this.getLayoutInflater();
+        View register_layout = inflater.inflate(R.layout.register_layout, null);
+
+        final MaterialEditText edt_name = register_layout.findViewById(R.id.edt_name);
+        final MaterialEditText edt_address = register_layout.findViewById(R.id.edt_address);
+        final MaterialEditText edt_birthdate = register_layout.findViewById(R.id.edt_birthdate);
+
+        Button btn_register = register_layout.findViewById(R.id.btn_register);
+
+        edt_birthdate.addTextChangedListener(new PatternedTextWatcher("####-##-##"));
+
+
+        builder.setView(register_layout);
+        final AlertDialog dialog = builder.create();
+
+        //Event
+        btn_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                dialog.dismiss();
+
+                if (TextUtils.isEmpty(edt_address.getText().toString()))
+                {
+                    Toast.makeText(MainActivity.this, "Please enter your address!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if (TextUtils.isEmpty(edt_birthdate.getText().toString()))
+                {
+                    Toast.makeText(MainActivity.this, "Please enter your date of birth!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if (TextUtils.isEmpty(edt_name.getText().toString()))
+                {
+                    Toast.makeText(MainActivity.this, "Please enter your name!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                final android.app.AlertDialog waitingDialog = new SpotsDialog(MainActivity.this);
+                waitingDialog.show();
+                waitingDialog.setMessage("Loading...");
+
+                mService.registerNewUser(phone,
+                        edt_name.getText().toString(),
+                        edt_address.getText().toString(),
+                        edt_birthdate.getText().toString())
+                        .enqueue(new Callback<User>() {
+                            @Override
+                            public void onResponse(Call<User> call, Response<User> response) {
+                                waitingDialog.dismiss();
+                                User user = response.body();
+                                if (TextUtils.isEmpty(user.getError_msg()))
+                                {
+                                    Toast.makeText(MainActivity.this, "User register successfully", Toast.LENGTH_SHORT).show();
+                                    //Start new Activity
+
+                                }
+                            }
+
+                            @Override
+                            public void onFailure(Call<User> call, Throwable t) {
+                                waitingDialog.dismiss();
+                            }
+                        });
+            }
+        });
+
+        dialog.show();
+    }
+
+    /*
+    Method to get the HASH Key from Facebook Developers
+     */
+    private void printKeyHash() {
+        try{
+            PackageInfo info = getPackageManager().getPackageInfo("com.vlasin.nicu.androiddrinkshop",
+                    PackageManager.GET_SIGNATURES);
+
+            for (Signature signature:info.signatures)
+            {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d("KEYHASH", Base64.encodeToString(md.digest(),Base64.DEFAULT));
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
     }
 }
--- app/src/main/java/com/vlasin/nicu/androiddrinkshop/Utils/Common.java
+++ app/src/main/java/com/vlasin/nicu/androiddrinkshop/Utils/Common.java
@@ -4,7 +4,7 @@ import com.vlasin.nicu.androiddrinkshop.Retrofit.IDrinkShopAPI;
 import com.vlasin.nicu.androiddrinkshop.Retrofit.RetrofitClient;
 
 public class Common {
-    private static final String BASE_URL = "http://localhost/drinkshop";
+    private static final String BASE_URL = "http://192.168.1.6/drinkshop/";
 
     public static IDrinkShopAPI getAPI()
     {
