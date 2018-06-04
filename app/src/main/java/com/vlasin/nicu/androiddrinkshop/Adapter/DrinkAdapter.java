caff0a96e3cbed85086ea4386bb7920206c06985
Nicu Vlasin <nicu_vlasin@yahoo.com>
2018-06-04 14:47:41 +0100

    Add to cart Dialog done

--- app/src/main/java/com/vlasin/nicu/androiddrinkshop/Adapter/DrinkAdapter.java
+++ app/src/main/java/com/vlasin/nicu/androiddrinkshop/Adapter/DrinkAdapter.java
@@ -1,14 +1,20 @@
 package com.vlasin.nicu.androiddrinkshop.Adapter;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RadioButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.cepheuen.elegantnumberbutton.view.ElegantNumberButton;
 import com.squareup.picasso.Picasso;
 import com.vlasin.nicu.androiddrinkshop.Interface.ItemClickListener;
 import com.vlasin.nicu.androiddrinkshop.Model.Drink;
@@ -34,10 +40,17 @@ public class DrinkAdapter extends RecyclerView.Adapter<DrinkViewHolder> {
     }
 
     @Override
-    public void onBindViewHolder(@NonNull DrinkViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull DrinkViewHolder holder, final int position) {
 
         holder.txt_price.setText(new StringBuilder("€").append(drinkList.get(position).Price).toString());
         holder.txt_drink_name.setText(drinkList.get(position).Name);
+
+        holder.btn_add_to_cart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showAddToCartDialog(position);
+            }
+        });
 
         Picasso.with(context)
                 .load(drinkList.get(position).Link)
@@ -50,6 +63,55 @@ public class DrinkAdapter extends RecyclerView.Adapter<DrinkViewHolder> {
             }
         });
     }
+
+    private void showAddToCartDialog(int position) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        View itemView = LayoutInflater.from(context)
+                .inflate(R.layout.add_to_cart_layout, null);
+
+        //View
+        ImageView img_product_dialog = itemView.findViewById(R.id.img_cart_product);
+        ElegantNumberButton txt_count = itemView.findViewById(R.id.txt_count);
+        TextView txt_product_dialog = itemView.findViewById(R.id.txt_cart_product_name);
+
+        EditText edt_comment = itemView.findViewById(R.id.edt_comment);
+
+        //Sizeof cup
+        RadioButton rdi_sizeM = itemView.findViewById(R.id.rdi_sizeM);
+        RadioButton rdi_sizeL = itemView.findViewById(R.id.rdi_sizeL);
+
+
+        //Sugar
+        RadioButton rdi_sugar_100 = itemView.findViewById(R.id.rdi_sugar_100);
+        RadioButton rdi_sugar_70 = itemView.findViewById(R.id.rdi_sugar_70);
+        RadioButton rdi_sugar_50 = itemView.findViewById(R.id.rdi_sugar_50);
+        RadioButton rdi_sugar_30 = itemView.findViewById(R.id.rdi_sugar_30);
+        RadioButton rdi_sugar_free = itemView.findViewById(R.id.rdi_sugar_free);
+
+        //Ice
+        RadioButton rdi_ice_100 = itemView.findViewById(R.id.rdi_ice_100);
+        RadioButton rdi_ice_70 = itemView.findViewById(R.id.rdi_ice_70);
+        RadioButton rdi_ice_50 = itemView.findViewById(R.id.rdi_ice_50);
+        RadioButton rdi_ice_30 = itemView.findViewById(R.id.rdi_ice_30);
+        RadioButton rdi_ice_free = itemView.findViewById(R.id.rdi_ice_free);
+
+        //Set data
+        Picasso.with(context)
+                .load(drinkList.get(position).Link)
+                .into(img_product_dialog);
+        txt_product_dialog.setText(drinkList.get(position).Name);
+
+        builder.setView(itemView);
+        builder.setNegativeButton("Add to Cart", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+
+        builder.show();
+    }
+
 
     @Override
     public int getItemCount() {
